983b2393f8af0e41c2201725958df0ef3dc1483b
vijay99 <vijay81238@gmail.com>
2022-03-21 11:09:07 +0530

    Create Reltio_SumOfPower.java

---
+++ src/com/interview/Reltio_SumOfPower.java
@@ -0,0 +1,38 @@
+package com.interviewquestion;
+
+/**
+ * @author Sacumen (www.sacumen.com)
+ * input 4,
+ *     2 the power = 1 ,
+ *     2 the power 1 =2 ,2*1
+ *      2 the power 2 =4,2*2
+ *       2 the power 3 =8,2*2*2
+ *        2 the power 4 =16,2*2*2*2
+ *
+ */
+//
+public class Reltio_SumOfPower {
+    public static void main(String[] args) {
+        int intputNumber = 4;
+        int sum = 0;
+
+       /* for (int i = 0; i < intputNumber; i++) {
+            if (i == 0)
+                sum = 1 + sum;
+            else
+                sum = (2 * i) + sum;
+
+        }*/
+
+        for(int i=0;i<intputNumber;i++){
+            for(int j=0;j<intputNumber;j++){
+               // sum=2*(getSum(j));
+            }
+        }
+        System.out.println("sum is =" + sum);
+    }
+
+
+    }
+
+
